761ace26d687511a9cce64ac5f60062abf628cdd
Jason Lengstorf <jason@lengstorf.com>
2022-08-02 13:29:30 -0700

    temp: don’t call the API so it can deploy

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,58 +5,70 @@ import styles from '../../styles/Home.module.css';
 
 export async function getStaticPaths() {
   // const url = new URL(process.env.URL || 'http://localhost:8888');
-  const url = new URL('http://localhost:8888');
-  url.pathname = '/api/products';
+  // const url = new URL('http://localhost:8888');
+  // url.pathname = '/api/products';
 
-  const res = await fetch(url.toString());
+  // const res = await fetch(url.toString());
 
-  if (!res.ok) {
-    console.error(res);
-    return { props: {} };
-  }
+  // if (!res.ok) {
+  //   console.error(res);
+  //   return { props: {} };
+  // }
 
-  const data = await res.json();
+  // const data = await res.json();
 
   return {
-    paths: data.products.edges.map(({ node }) => `/product/${node.handle}`),
+    // paths: data.products.edges.map(({ node }) => `/product/${node.handle}`),
+    paths: ['/product/test'],
     fallback: true,
   };
 }
 
 export async function getStaticProps({ params }) {
   // const url = new URL(process.env.URL || 'http://localhost:8888');
-  const url = new URL('http://localhost:8888');
-  url.pathname = '/api/products';
+  // const url = new URL('http://localhost:8888');
+  // url.pathname = '/api/products';
 
-  const res = await fetch(url.toString());
+  // const res = await fetch(url.toString());
 
-  if (!res.ok) {
-    console.error(res);
-    return { props: {} };
-  }
+  // if (!res.ok) {
+  //   console.error(res);
+  //   return { props: {} };
+  // }
 
-  const data = await res.json();
+  // const data = await res.json();
 
-  const product = data.products.edges
-    .map(({ node }) => {
-      if (node.totalInventory <= 0) {
-        return false;
-      }
+  // const product = data.products.edges
+  //   .map(({ node }) => {
+  //     if (node.totalInventory <= 0) {
+  //       return false;
+  //     }
 
-      return {
-        id: node.id,
-        title: node.title,
-        description: node.description,
-        imageSrc: node.images.edges[0].node.src,
-        imageAlt: node.title,
-        price: node.variants.edges[0].node.priceV2.amount,
-        slug: node.handle,
-      };
-    })
-    .find(({ slug }) => slug === params.slug);
+  //     return {
+  //       id: node.id,
+  //       title: node.title,
+  //       description: node.description,
+  //       imageSrc: node.images.edges[0].node.src,
+  //       imageAlt: node.title,
+  //       price: node.variants.edges[0].node.priceV2.amount,
+  //       slug: node.handle,
+  //     };
+  //   })
+  //   .find(({ slug }) => slug === params.slug);
 
   return {
-    props: { product },
+    props: {
+      product: {
+        id: 'a1',
+        title: 'Test',
+        description: 'Test',
+        imageSrc:
+          'https://cdn.shopify.com/s/files/1/0589/5798/8049/products/corgi-toy.jpg',
+        imageAlt: 'test',
+        price: '19.99',
+        slug: 'test',
+      },
+    },
   };
 }
 
