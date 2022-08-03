f9880d4e44b1112b05575eea7dc20ebea9e0b1b2
Jason Lengstorf <jason@lengstorf.com>
2022-08-03 12:29:59 -0700

    feat: use the API now that it’s deployed

--- pages/index.js
+++ pages/index.js
@@ -4,53 +4,55 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export async function getStaticProps() {
-  // const url = new URL(process.env.URL || 'http://localhost:8888');
-  // const url = new URL('http://localhost:8888');
-  // url.pathname = '/api/products';
+  const url = new URL(process.env.URL || 'http://localhost:8888');
+  url.pathname = '/api/products';
 
-  // const res = await fetch(url.toString());
+  const res = await fetch(url.toString());
 
-  // if (!res.ok) {
-  //   console.error(res);
-  //   return { props: {} };
-  // }
+  if (!res.ok) {
+    console.error(res);
+    return { props: {} };
+  }
 
-  // const data = await res.json();
+  const data = await res.json();
 
-  // const products = data.products.edges
-  //   .map(({ node }) => {
-  //     if (node.totalInventory <= 0) {
-  //       return false;
-  //     }
+  const products = data.products.edges
+    .map(({ node }) => {
+      if (node.totalInventory <= 0) {
+        return false;
+      }
 
-  //     return {
-  //       id: node.id,
-  //       title: node.title,
-  //       description: node.description,
-  //       imageSrc: node.images.edges[0].node.src,
-  //       imageAlt: node.title,
-  //       price: node.variants.edges[0].node.priceV2.amount,
-  //       slug: node.handle,
-  //     };
-  //   })
-  //   .filter(Boolean);
+      return {
+        id: node.id,
+        title: node.title,
+        description: node.description,
+        imageSrc: node.images.edges[0].node.src,
+        imageAlt: node.title,
+        price: node.variants.edges[0].node.priceV2.amount,
+        slug: node.handle,
+      };
+    })
+    .filter(Boolean);
 
   return {
-    // props: { products },
-    props: {
-      products: [
-        {
-          id: 'a1',
-          title: 'Test',
-          description: 'Test',
-          imageSrc:
-            'https://cdn.shopify.com/s/files/1/0589/5798/8049/products/corgi-toy.jpg',
-          imageAlt: 'test',
-          price: '19.99',
-          slug: 'test',
-        },
-      ],
-    },
+    props: { products },
+    // In case you're building this yourself, the first deployment can't call
+    // the API because it hasn't been deployed yet. This dummy product will get
+    // you through that first deploy.
+    // props: {
+    //   products: [
+    //     {
+    //       id: 'a1',
+    //       title: 'Test',
+    //       description: 'Test',
+    //       imageSrc:
+    //         'https://cdn.shopify.com/s/files/1/0589/5798/8049/products/corgi-toy.jpg',
+    //       imageAlt: 'test',
+    //       price: '19.99',
+    //       slug: 'test',
+    //     },
+    //   ],
+    // },
   };
 }
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,71 +4,76 @@ import Link from 'next/link';
 import styles from '../../styles/Home.module.css';
 
 export async function getStaticPaths() {
-  // const url = new URL(process.env.URL || 'http://localhost:8888');
-  // const url = new URL('http://localhost:8888');
-  // url.pathname = '/api/products';
+  const url = new URL(process.env.URL || 'http://localhost:8888');
+  url.pathname = '/api/products';
 
-  // const res = await fetch(url.toString());
+  const res = await fetch(url.toString());
 
-  // if (!res.ok) {
-  //   console.error(res);
-  //   return { props: {} };
-  // }
+  if (!res.ok) {
+    console.error(res);
+    return { props: {} };
+  }
 
-  // const data = await res.json();
+  const data = await res.json();
 
   return {
-    // paths: data.products.edges.map(({ node }) => `/product/${node.handle}`),
-    paths: ['/product/test'],
+    paths: data.products.edges.map(({ node }) => `/product/${node.handle}`),
+    // In case you're building this yourself, the first deployment can't call
+    // the API because it hasn't been deployed yet. This test path will get you
+    // through that first deploy.
+    // paths: ['/product/test'],
     fallback: true,
   };
 }
 
 export async function getStaticProps({ params }) {
-  // const url = new URL(process.env.URL || 'http://localhost:8888');
-  // const url = new URL('http://localhost:8888');
-  // url.pathname = '/api/products';
+  const url = new URL(process.env.URL || 'http://localhost:8888');
+  url.pathname = '/api/products';
 
-  // const res = await fetch(url.toString());
+  const res = await fetch(url.toString());
 
-  // if (!res.ok) {
-  //   console.error(res);
-  //   return { props: {} };
-  // }
+  if (!res.ok) {
+    console.error(res);
+    return { props: {} };
+  }
 
-  // const data = await res.json();
+  const data = await res.json();
 
-  // const product = data.products.edges
-  //   .map(({ node }) => {
-  //     if (node.totalInventory <= 0) {
-  //       return false;
-  //     }
+  const product = data.products.edges
+    .map(({ node }) => {
+      if (node.totalInventory <= 0) {
+        return false;
+      }
 
-  //     return {
-  //       id: node.id,
-  //       title: node.title,
-  //       description: node.description,
-  //       imageSrc: node.images.edges[0].node.src,
-  //       imageAlt: node.title,
-  //       price: node.variants.edges[0].node.priceV2.amount,
-  //       slug: node.handle,
-  //     };
-  //   })
-  //   .find(({ slug }) => slug === params.slug);
+      return {
+        id: node.id,
+        title: node.title,
+        description: node.description,
+        imageSrc: node.images.edges[0].node.src,
+        imageAlt: node.title,
+        price: node.variants.edges[0].node.priceV2.amount,
+        slug: node.handle,
+      };
+    })
+    .find(({ slug }) => slug === params.slug);
 
   return {
-    props: {
-      product: {
-        id: 'a1',
-        title: 'Test',
-        description: 'Test',
-        imageSrc:
-          'https://cdn.shopify.com/s/files/1/0589/5798/8049/products/corgi-toy.jpg',
-        imageAlt: 'test',
-        price: '19.99',
-        slug: 'test',
-      },
-    },
+    props: { product },
+    // In case you're building this yourself, the first deployment can't call
+    // the API because it hasn't been deployed yet. This dummy product will get
+    // you through that first deploy.
+    // props: {
+    //   product: {
+    //     id: 'a1',
+    //     title: 'Test',
+    //     description: 'Test',
+    //     imageSrc:
+    //       'https://cdn.shopify.com/s/files/1/0589/5798/8049/products/corgi-toy.jpg',
+    //     imageAlt: 'test',
+    //     price: '19.99',
+    //     slug: 'test',
+    //   },
+    // },
   };
 }
 
@@ -91,7 +96,6 @@ function Product({ slug, imageSrc, imageAlt, title, description, price }) {
 }
 
 export default function ProductPage({ product }) {
-  console.log({ product });
   return (
     <div className={styles.container}>
       <Head>
